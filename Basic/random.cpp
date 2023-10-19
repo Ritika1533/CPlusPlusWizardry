095f91416cf2a8cee875c44fae5b2da29feca73f
unknown <ritikakes33@gmail.com>
2023-10-19 21:03:37 +0530

    learning

---
+++ Basic/random.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+        cout << rand() << endl;
+    cout << endl;
+    return 0;
+}
