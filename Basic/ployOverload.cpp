53cd47e2c2328d0039540efdc5a49491db9859bb
unknown <ritikakes33@gmail.com>
2023-12-08 23:42:02 +0530

    practice

---
+++ Basic/ployOverload.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+class polyOverload {
+public:
+
+    void func(int x)
+    {
+        cout << "value of x is " << x << endl;
+    }
+
+    void func(double x)
+    {
+        cout << "value of x is " << x << endl;
+    }
+ 
+    void func(int x, int y)
+    {
+        cout << "value of x and y is " << x << ", " << y
+             << endl;
+    }
+};
+ 
+
+int main()
+{
+    polyOverload obj1;
+
+    obj1.func(7);
+
+    obj1.func(9.132);
+
+    obj1.func(85, 64);
+    return 0;
+}
